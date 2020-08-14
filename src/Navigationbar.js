4785ee90dc8fe6686642adc703cdb41614e72bb2
Kyle McNeese <kjmcneese@gmail.com>
2020-08-14 18:16:30 -0500

    misc changes

--- src/Navigationbar.js
+++ src/Navigationbar.js
@@ -10,14 +10,14 @@ function NavigationBar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="#home">Ceremony</Nav.Link>
-                <Nav.Link href="#link">Reception</Nav.Link>
+                <Nav.Link href="#festivities">Festivities</Nav.Link>
+                <Nav.Link href="#accommodations">Accommodations</Nav.Link>
                 <Navbar.Brand href="#home">Kyle & Lauren</Navbar.Brand>
-                <Nav.Link href="#link">Registry</Nav.Link>
+                <Nav.Link href="#registry">Registry</Nav.Link>
                 <NavDropdown title="Photos" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Proposal</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Engagement</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Wedding</NavDropdown.Item>
+                    <NavDropdown.Item href="#proposalPhotos">Proposal</NavDropdown.Item>
+                    <NavDropdown.Item href="#engagementPhotos">Engagement</NavDropdown.Item>
+                    <NavDropdown.Item href="#weddingPhotos">Wedding</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
             </Navbar.Collapse>
