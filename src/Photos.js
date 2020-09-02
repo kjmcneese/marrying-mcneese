173cfa2a845dfdb56e31de76a9df6ce59340f0d3
Kyle McNeese <kjmcneese@gmail.com>
2020-09-01 20:27:33 -0500

    photos in website

--- src/Photos.js
+++ src/Photos.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import './css/Photos.css';
 
 var pageTitle = "Photos";
 
-function Photos() {
-    return (
-        <div>
-            <h3>{pageTitle}</h3>
-        </div>
-    );
+class Photos extends React.Component {
+    render() {
+        const array = ["IMG_1526", "IMG_1527", "IMG_1529", "IMG_1535", "IMG_7695"];
+        const images = array.map(image => {
+            return <Image key={image} src={require(`./images/proposal/${image}.JPEG`)} className="photo" rounded />
+        });
+
+        return (
+            <div className="photos">
+                <h3>{pageTitle}</h3>
+                <Tabs defaultActiveKey="proposal">
+                    <Tab eventKey="proposal" title="Proposal">
+                        <div>
+                            {images}
+                        </div>
+                    </Tab>
+                    <Tab eventKey="engagement" title="Engagement" disabled>
+                        Coming soon!
+                    </Tab>
+                    <Tab eventKey="wedding" title="Wedding" disabled>
+                        Coming soon!
+                    </Tab>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
 export default Photos;
