dd1e3572d229cb42d470649bb9db99db15430095
Kyle McNeese <kjmcneese@gmail.com>
2020-09-04 21:14:18 -0500

    created loop for route components in body

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -4,26 +4,26 @@ import { Route, Switch } from 'react-router-dom';
 import '../css/AppBody.scss';
 
 import Home from './Home';
+// These route components should be named generically for better project reuse
 import Accommodations from './Accommodations';
 import Registry from './Registry';
 import Photos from './Photos';
 
+
 class AppBody extends React.Component {
     render() {
-        // How to use this loop generically when component names are different?
-        // Can rename components as generic names (ex. RouteSection1, RouteSection2, etc.), but names are still different.
-        // const routeSections = [];
-        // for (var routeTitle of this.props.appData.routeTitles) {
-        //     routeSections.push(<Route path={ "/" + routeTitle } component={Accommodations} />);
-        // }
+        const routeComponentList = [Accommodations, Registry, Photos];
+        const routeSections = [];
+        for (var i = 0; i < this.props.appData.routeTitles.length; i++) {
+            var routeTitle = this.props.appData.routeTitles[i];
+            routeSections.push(<Route path={ "/" + routeTitle } component={ routeComponentList[i] } key={ routeTitle }/>);
+        }
 
         return (
             <div className="appBody">
                 <Switch>
                     <Route path="/" component={Home} exact />
-                    <Route path={ "/" + this.props.appData.routeTitles[0] } component={Accommodations} />
-                    <Route path={ "/" + this.props.appData.routeTitles[1] } component={Registry} />
-                    <Route path={ "/" + this.props.appData.routeTitles[2] } component={Photos} />
+                    { routeSections }
                 </Switch>
             </div>
         );
