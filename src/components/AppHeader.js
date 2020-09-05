56f393ebcd9f7a546b7cbc3d4af06bad284b21bd
Kyle McNeese <kjmcneese@gmail.com>
2020-09-04 20:25:47 -0500

    completed organizing AppHeader component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,21 +3,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import '../css/AppHeader.scss';
 
-const anchorTitles = ["Wedding", "Tinsmith", "Schedule"];
-const anchorLinks = [];
-for (let i = 0; i < anchorTitles.length; i++) {
-    anchorLinks.push(<Nav.Link href={ "/#" + anchorTitles[i] } className="navigationBarText">{ anchorTitles[i] }</Nav.Link>);
-}
-
-const routeTitles = ["Accommodations", "Registry", "Photos"];
-const routeLinks = [];
-for (let i = 0; i < routeTitles.length; i++) {
-    routeLinks.push(<Nav.Link href={ "/" + routeTitles[i] } className="navigationBarText">{ routeTitles[i] }</Nav.Link>);
-}
-
-
 class AppHeader extends React.Component {
     render() {
+        const anchorLinks = [];
+        for (var anchorTitle of this.props.appData.anchorTitles) {
+            anchorLinks.push(<Nav.Link href={ "/#" + anchorTitle } className="navigationBarText">{ anchorTitle }</Nav.Link>);
+        }
+
+        const routeLinks = [];
+        for (var routeTitle of this.props.appData.routeTitles) {
+            routeLinks.push(<Nav.Link href={ "/" + routeTitle } className="navigationBarText">{ routeTitle }</Nav.Link>);
+        }
+
         return (
             <header className="appHeader">
                 <Navbar className="navigationBar" expand="md">
