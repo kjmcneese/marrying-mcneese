baee9b7b6dff91712fd23ff7628145e69af94a29
Kyle McNeese <kjmcneese@gmail.com>
2020-09-07 15:04:13 -0500

    figured out dynamic, purposed components for carousel

--- src/components/homeSections/CustomCarousel.js
+++ src/components/homeSections/CustomCarousel.js
@@ -7,11 +7,19 @@ import '../../css/CustomCarousel.scss';
 
 class CustomCarousel extends React.Component {
     render() {
+        const imageList = [love, love, love];
+        const carouselItems = [];
+        for (var i = 0; i < imageList.length; i++) {
+            carouselItems.push(
+                <Carousel.Item key={ "carouselItem" + i }>
+                    <CustomCarouselItem hashtag={ this.props.hashtag } weddingDate={ this.props.weddingDate } image={ imageList[i] } />
+                </Carousel.Item>
+            );
+        }
+
         return (
             <Carousel>
-                <CustomCarouselItem hashtag={ this.props.hashtag } weddingDate={ this.props.weddingDate } image={ love }/>
-                <CustomCarouselItem hashtag={ this.props.hashtag } weddingDate={ this.props.weddingDate } image={ love }/>
-                <CustomCarouselItem hashtag={ this.props.hashtag } weddingDate={ this.props.weddingDate } image={ love }/>
+                { carouselItems }
             </Carousel>
         );
     }
--- src/components/reusable/CustomCarouselItem.js
+++ src/components/reusable/CustomCarouselItem.js
@@ -6,13 +6,13 @@ import '../../css/CustomCarouselItem.scss';
 class CustomCarouselItem extends React.Component {
     render() {
         return (
-            <Carousel.Item>
-                <img className="d-block w-100" src={ this.props.love } alt="First slide"/>
+            <div>
+                <img className="d-block w-100" src={ this.props.image } alt="First slide"/>
                 <Carousel.Caption>
                     <h4>{ this.props.hashtag }</h4>
                     <h4>{ this.props.weddingDate }</h4>
                 </Carousel.Caption>
-            </Carousel.Item>
+            </div>
         );
     }
 }
