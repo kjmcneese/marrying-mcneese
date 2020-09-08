b098a30c033e2b0d51114cb25496902d53387625
Kyle McNeese <kjmcneese@gmail.com>
2020-09-08 15:50:41 -0500

    added engagement photos

--- src/components/routes/Photos.js
+++ src/components/routes/Photos.js
@@ -5,30 +5,40 @@ import Tab from 'react-bootstrap/Tab';
 import Image from 'react-bootstrap/Image';
 import '../../css/Photos.scss';
 
-var pageTitle = "Photos";
+let photos = require('../../json/photos.json');
 
 class Photos extends React.Component {
     render() {
-        const array = ["IMG_1526", "IMG_1527", "IMG_1529", "IMG_1535", "IMG_7695"];
-        const images = array.map(image => {
-            return <Image key={ image } src={require(`../../images/proposal/${ image }.JPEG`)} className="photo" rounded />
+        const pageNotice = "Professional photography by the lovely Mo Speer";
+
+        const proposalImageNames = photos[Object.keys(photos)[0]].Proposal;
+        const proposalImages = proposalImageNames.map(proposalImageName => {
+            return <Image key={ proposalImageName } src={require(`../../images/proposal/${ proposalImageName }.JPEG`)} className="photo" rounded />
+        });
+
+        const engagementImageNames = photos[Object.keys(photos)[0]].Engagement;
+        const engagementImages = engagementImageNames.map(engagementImageName => {
+            return <Image key={ engagementImageName } src={require(`../../images/engagement/${ engagementImageName }.jpg`)} className="photo" rounded />
+        });
+
+        const weddingImageNames = photos[Object.keys(photos)[0]].Wedding;
+        const weddingImages = weddingImageNames.map(weddingImageName => {
+            return <Image key={ weddingImageName } src={require(`../../images/wedding/${ weddingImageName }.JPEG`)} className="photo" rounded />
         });
 
         return (
-            <div className="photos">
-                <RouteTop pageTitle={ pageTitle } />
+            <div>
+                <RouteTop pageTitle={ Object.keys(photos)[0] } pageNotice={ pageNotice } />
 
-                <Tabs defaultActiveKey="proposal">
-                    <Tab eventKey="proposal" title="Proposal">
-                        <div>
-                            { images }
-                        </div>
+                <Tabs className="photos" defaultActiveKey="proposal">
+                    <Tab className="photos" eventKey="proposal" title="Proposal">
+                        { proposalImages }
                     </Tab>
-                    <Tab eventKey="engagement" title="Engagement" disabled>
-                        Coming soon!
+                    <Tab className="photos" eventKey="engagement" title="Engagement">
+                        { engagementImages }
                     </Tab>
-                    <Tab eventKey="wedding" title="Wedding" disabled>
-                        Coming soon!
+                    <Tab className="photos" eventKey="wedding" title="Wedding" disabled>
+                        { weddingImages }
                     </Tab>
                 </Tabs>
             </div>
