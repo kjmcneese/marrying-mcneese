777b3dde200a6ee320e483f8e268e540fe340dd3
Kyle McNeese <kjmcneese@gmail.com>
2020-09-20 15:30:04 -0500

    initial configuration and firebase document add setup

--- src/components/routes/FormRSVP.js
+++ src/components/routes/FormRSVP.js
@@ -2,33 +2,66 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-function FormRSVP() {
-    return (
-        <Form>
-            <Form.Group controlId="formPersonName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control placeholder="Enter name" />
-            </Form.Group>
+import addRSVP from '../../services/firebaseConfig';
 
-            <Form.Group controlId="formAttending">
-                <Form.Label>Attending</Form.Label>
-                <Form.Check type="radio" id="attendingYes" name="attending" label="Yes" value={ 1 } />
-                <Form.Check type="radio" id="attendingNo" name="attending" label="No" value={ 0 } />
-            </Form.Group>
+class FormRSVP extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            Name: "",
+            Attending: false,
+            Meal: ""
+        }
 
+        this.updateName = this.updateName.bind(this);
+        this.updateAttending = this.updateAttending.bind(this);
+        this.submitRSVP = this.submitRSVP.bind(this);
+    }
 
-            {/* <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
+    updateName(e) {
+        this.setState({Name: e.target.value});
+    }
 
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group> */}
+    updateAttending(e) {
+        this.setState({Attending: !this.state.Attending});
+    }
 
-            <Button className="rsvpSubmitButton" type="submit" disabled>Submit</Button>
-        </Form>
-    );
+    submitRSVP() {
+        addRSVP({
+            Name: this.state.Name,
+            Attending: this.state.Attending,
+            Meal: this.state.Meal
+        });
+    }    
+
+    render() {
+        return (
+            <Form>
+                <Form.Group controlId="formPersonName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control placeholder="Enter name" value={ this.state.Name } onChange={ this.updateName }/>
+                </Form.Group>
+    
+                <Form.Group controlId="formAttending">
+                    <Form.Label>Attending</Form.Label>
+                    <Form.Check type="radio" id="attendingYes" name="attending" label="Yes" value={ this.state.Attending } onChange={ this.updateAttending } />
+                    <Form.Check type="radio" id="attendingNo" name="attending" label="No" value={ this.state.Attending } onChange={ this.updateAttending } />
+                </Form.Group>
+    
+    
+                {/* <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+    
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+    
+                <Button className="rsvpSubmitButton" onClick={ this.submitRSVP }>Submit</Button>
+            </Form>
+        );
+    }
 }
 
 export default FormRSVP;
