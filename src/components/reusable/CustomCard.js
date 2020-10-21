2c30c08dac4f4f9e82d77de3a9171d02b3447173
Kyle McNeese <kjmcneese@gmail.com>
2020-10-20 19:25:52 -0500

    change

--- src/components/reusable/CustomCard.js
+++ src/components/reusable/CustomCard.js
@@ -7,6 +7,7 @@ import Constants from '../../Constants';
 let city = require('../../json/appData.json').city;
 
 class CustomCard extends React.Component {
+
   render() {
     return (
       <Card>
